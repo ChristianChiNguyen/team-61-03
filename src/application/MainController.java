f3aed0559be944316deaf579bdb5d3b5cc6837f9
Ellie Mansour <mansour.eilbera@gmail.com>
2023-07-14 18:13:43 -0700

    Ellie Updates to Logout

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -3,7 +3,12 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.*;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainController implements Initializable{
 
@@ -12,5 +17,25 @@ public class MainController implements Initializable{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	 @FXML
+	    public void Logout(ActionEvent event) throws Exception {
+	        // get the current stage
+	        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-}
+	        // close the current (Main) window
+	        stage.close();
+
+	        // open the Login window
+	        Stage loginStage = new Stage();
+	        Parent root = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
+	        Scene scene = new Scene(root);
+	        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	        loginStage.setScene(scene);
+	        loginStage.show();
+	    }
+	
+
+	}
+
+
